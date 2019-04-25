430e6c409adc478ce102d9ba4061510ee9332918
Leeya Davis <leeya463@gmail.com>
2019-04-25 22:17:55 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -79,7 +79,7 @@ class MusicLibraryController
       puts "Which song number would you like to play?"
       input = gets.strip.to_i
       #binding.pry
-      song = Song.all[input-1]
+      song = Song.all[input]
       input > 0 && input <= Song.all.size - 1 ? song : nil#ensures input is valid
 
 #upon receiving valid input 'plays' the matching song from the alphabetized list output by #list_songs
