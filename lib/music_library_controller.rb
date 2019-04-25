751d94167c438eaac31bcf69be316d0045f4ad1f
Leeya Davis <leeya463@gmail.com>
2019-04-25 22:43:30 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -65,7 +65,6 @@ class MusicLibraryController
     end
 
     def play_song
-      #list_songs
       puts "Which song number would you like to play?"
       input = gets.strip.to_i
       #binding.pry
